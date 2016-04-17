80c47b15a65c4fb7d03ea022dba4c27c4bd8997a
Peter <peter.nagy@crosssec.com>
2016-04-17 20:12:24 +0100

    User and common module set

--- config/application.config.php
+++ config/application.config.php
@@ -1,7 +1,8 @@
 <?php
 return array(
     'modules' => array(
-        'Common'
+        'Common',
+        'User'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
